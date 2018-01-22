97d1490e0a4bb1f360935c9e520dc6eef375bc13
Dwiki Nurkurniawan <nurkurniawandwiki@gmail.com>
2018-01-22 16:05:28 +0700

    Preprocessing, gausah bikin fungsi dulu, nanti ama urang dibikin fungsinya belakangan

--- preprocessing.py
+++ preprocessing.py
@@ -1,38 +1,49 @@
 import csv
-import time
 
 import numpy as np
-start = time.time()
+import pywt
 
-data = np.array(['F3','FC5','F7','T7','P7','O1','O2','P8','T8','F8','AF4','FC6','F4','AF3'])
-
-with open('hehe.csv', mode='r') as inputFile:
-	reader = csv.reader(inputFile)
-	#row_count = sum(1 for row in reader)
-	#print row_count
-	hasil = []
-	hasilmax = 0
+#get_raw_data
+header = ['F3','FC5','F7','T7','P7','O1','O2','P8','T8','F8','AF4','FC6','F4','AF3']
+with open('aihihi.csv', mode='r') as input_file:
+	# Baca file csv
+	reader = csv.reader(input_file)
+	# inisialisasi array awal serta header
+	hasil = np.array(header)
+	# untuk menghilangkan header yang ada di csv
+	First = True
+	# looping ke bawah 
 	for row in reader:
-	 	temp = []
-	 	for col in xrange(0,len(row)):
-	 		if col%2 != 0:
-	 			temp.append(row[col])
-	 			hasilmax += 1
-			if hasilmax < 5:
-		 		hasil.append(temp)
-	 		# print col
-	# 	print temp
-	# print max(hasil)
+		# data per baris (ambil data yang penting (sinyal) )
+		temp = []
+		for col in xrange(0,len(row)):
+			if col%2 != 0:
+				# kalo baris pertama(header) maka di break
+				if First:
+					break
+				# kalo udah 14 item maka break
+				if len(temp) > 13:
+					break
+				# digabung biar bisa jadi array
+				temp = np.append(temp, row[col])
 
-	data = np.append(data, hasil)
-	print data
-	res = time.time() - start
-	print "Time Elapsed : %s" % res
-	# print data
-# for row in inputFile:
-# 	tstamp = row["Timestamp"]
-# 	F3 = row["F3 Value"]
+		# kalo bukan pertama maka ditambah ke array, kalo pertama maka set jadi false
+ 		if not First:
+			hasil = np.vstack((hasil, np.array(temp)))
+		else:
+			First = False
 
-# 	# print "%s\t|\t%s" % (tstamp, F3)
-# 	print row
-# 	break
+# print hasil
+
+# normalisasi
+i = 0
+for column in hasil.T:
+	hasil.T[i][1:] = hasil.T[i][1:].astype(np.float) - np.mean(column[1:].astype(np.float))
+	i += 1
+
+# print "\nsetelah normalisasi"
+# print hasil
+
+# wavelet
+# for column in hasil.T:
+# 	cA, cD = pywt.dwt(column, 'db5')
