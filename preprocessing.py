22936e63a80edba8802f25d073c4fd8d04cefca2
Dwiki Nurkurniawan <nurkurniawandwiki@gmail.com>
2018-01-23 14:15:49 +0700

    tambah timestamp sekalian ganti algoritma ambil data, tambah pandas di venv

--- preprocessing.py
+++ preprocessing.py
@@ -1,39 +1,16 @@
 import csv
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 # import pywt
 from scipy.signal import butter, lfilter
 from scipy.fftpack import rfft, irfft, fftfreq
 
 #get_raw_data
-header = ['F3','FC5','F7','T7','P7','O1','O2','P8','T8','F8','AF4','FC6','F4','AF3']
-with open('aihihi.csv', mode='r') as input_file:
-	# Baca file csv
-	reader = csv.reader(input_file)
-	# inisialisasi array awal serta header
-	hasil = np.array(header)
-	# untuk menghilangkan header yang ada di csv
-	First = True
-	# looping ke bawah 
-	for row in reader:
-		# data per baris (ambil data yang penting (sinyal) )
-		temp = []
-		for col in xrange(0,len(row)):
-			if col%2 != 0:
-				# kalo baris pertama(header) maka di break
-				if First:
-					break
-				# kalo udah 14 item maka break
-				if len(temp) > 13:
-					break
-				# digabung biar bisa jadi array
-				temp = np.append(temp, row[col])
-
-		# kalo bukan pertama maka ditambah ke array, kalo pertama maka set jadi false
- 		if not First:
-			hasil = np.vstack((hasil, np.array(temp)))
-		else:
-			First = False
+header = ['Timestamp', 'F3 Value','FC5 Value','F7 Value','T7 Value','P7 Value','O1 Value','O2 Value','P8 Value','T8 Value','F8 Value','AF4 Value','FC6 Value','F4 Value','AF3 Value']
+hasil = np.array(header)
+hasil = np.vstack((hasil, pd.read_csv('aihihi.csv', skipinitialspace=True, usecols=header).as_matrix()))
 
 # normalisasi
 # Sample rate dan cutoff (dalam hz)
@@ -45,8 +22,8 @@ T = 1/256
 order = 5
 
 # (DC offset)
-i = 0
-for column in hasil.T:
+i = 1
+for column in hasil.T[1:]:
 	hasil.T[i][1:] = hasil.T[i][1:].astype(np.float) - np.mean(column[1:].astype(np.float))	
 	i += 1
 
