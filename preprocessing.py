1194ce56af38b8b32d7b5b1b0dc6c03d85545ad3
Fazlur Rahman <fazlur97@gmail.com>
2018-01-26 10:32:14 +0700

    pre class

--- preprocessing.py
+++ preprocessing.py
@@ -1,61 +1,65 @@
 import csv
-
 import numpy as np
 import scipy, pylab
 import matplotlib.pyplot as plt
-# import pywt
 from scipy.signal import butter, lfilter
 from scipy.fftpack import rfft, irfft, fftfreq
 
 #get_raw_data
-header = ['Timestamp', 'F3 Value','FC5 Value','F7 Value','T7 Value','P7 Value','O1 Value','O2 Value','P8 Value','T8 Value','F8 Value','AF4 Value','FC6 Value','F4 Value','AF3 Value']
-hasil = np.array(header)
-with open('aihihi.csv', 'r') as f:
-	reader = csv.DictReader(f)
-	for row in reader:
-		row_text = []
-		for col in header:
-			row_text = np.append(row_text, row[col])
-		hasil = np.vstack((hasil, row_text))
+class preprocessing:
+	
+	def __init__(self, name):
+		self.name = name
+		self.header = ['Timestamp', 'F3 Value','FC5 Value','F7 Value','T7 Value','P7 Value','O1 Value','O2 Value','P8 Value','T8 Value','F8 Value','AF4 Value','FC6 Value','F4 Value','AF3 Value']
+		self.hasil = np.array(self.header)
+		self.ibp=[0]
+		print("Membaca {}".format(self.name))
 
-# normalisasi
-# Sample rate dan cutoff (dalam hz)
+	def readData(self):
+		
+		with open(self.name, 'r') as f:
+			reader = csv.DictReader(f)
+			for row in reader:
+				row_text = []
+				for col in self.header:
+					row_text = np.append(row_text, row[col])
+				self.hasil = np.vstack((self.hasil, row_text))
 
-# (DC offset)
-ibp=[0]
-i = 1
-for column in hasil.T[1:]:
-    hasil.T[i][1:] = hasil.T[i][1:].astype(np.float) - np.mean(column[1:].astype(np.float))	
-    fft = scipy.fft(hasil.T[i][1:])
-    bp = fft[:]
-    for j in range(len(bp)):
-        if j>=10:bp[j]=0
-    ibp.append(scipy.ifft(bp))
-    i += 1
-# FFT
+	def fft(self):
+		# normalisasi
+		# (DC offset)
+		i = 1
+		for column in self.hasil.T[1:]:
+			self.hasil.T[i][1:] = self.hasil.T[i][1:].astype(np.float) - np.mean(column[1:].astype(np.float))	#DC Offset    
+		# FFT
+			fft = scipy.fft(self.hasil.T[i][1:])
+			bp = fft[:]
+			for j in range(len(bp)):
+				if j>=10:bp[j]=0
+			self.ibp.append(scipy.ifft(bp))
+			i += 1
 
+	def plot(self):
+		h,w=3,2
+		pylab.figure(figsize=(12,9))
+		pylab.subplots_adjust(hspace=.7)
+		pylab.subplot(h,w,1);pylab.title("(I) Sinyal Asli")
+		pylab.plot(self.hasil.T[1][1:])
+		pylab.show()
+# pylab.subplot(h,w,2);pylab.title("(I) Sinyal FFT")
+# pylab.plot(ibp[1])
 
-print ibp[0]
-print '=-------------='
+# pylab.subplot(h,w,3);pylab.title("(I) Sinyal FFT")
+# pylab.plot(hasil.T[2][1:])
 
-h,w=3,2
-pylab.figure(figsize=(12,9))
-pylab.subplots_adjust(hspace=.7)
+# pylab.subplot(h,w,4);pylab.title("(I) Sinyal FFT")
+# pylab.plot(ibp[2])
 
-pylab.subplot(h,w,1);pylab.title("(I) Sinyal Asli")
-pylab.plot(hasil.T[1][1:])
-
-pylab.subplot(h,w,2);pylab.title("(I) Sinyal FFT")
-pylab.plot(ibp[1])
-
-pylab.subplot(h,w,3);pylab.title("(I) Sinyal FFT")
-pylab.plot(hasil.T[2][1:])
-
-pylab.subplot(h,w,4);pylab.title("(I) Sinyal FFT")
-pylab.plot(ibp[2])
-
-pylab.show()
-
+# pylab.show()
+babi1=preprocessing('aihihi.csv')
+babi1.readData()
+babi1.fft()
+babi1.plot()
 
 # # Fungsi Bandpass
 # def butter_bandpass(lowcut, highcut, fs, order=5):
