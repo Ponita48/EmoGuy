b23530c26938d3f071609e8764cdca35ae63127d
hasemeneh <julviar17@gmail.com>
2018-01-30 09:04:47 +0700

    Desktop apps and socket

---
+++ Desktop/EmoGuyWPF/EmoGuyWPF/Model/SensorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmoGuyWPF.Model
+{
+	public class SensorModel
+	{
+		double _quality, _value;
+
+		public double quality { get => _quality; set => _quality = value; }
+		public double value { get => _value; set => _value = value; }
+	}
+}
