5dc9747ae9c9fc2eda7ffe40bd459b92092c802f
Dwiki Nurkurniawan <nurkurniawandwiki@gmail.com>
2018-02-01 16:02:12 +0700

    TITIT KUDA

--- preprocessing.py
+++ preprocessing.py
@@ -12,6 +12,7 @@ import processing
 class preprocessing:
 	
 	def __init__(self, name):
+		start = time.time()
 		self.name = name
 		self.header = ['Timestamp', 'F3 Value','FC5 Value','F7 Value','T7 Value','P7 Value','O1 Value','O2 Value','P8 Value','T8 Value','F8 Value','AF4 Value','FC6 Value','F4 Value','AF3 Value']
 		self.hasil = np.array(self.header)
@@ -22,26 +23,29 @@ class preprocessing:
 		if self.name[-3:] == "csv" or self.name[-3:] == "CSV":
 			print "Reading csv file: %s" % self.name
 			self.readData()
+			print time.time() - start
 			self.dcOffset()
 		elif self.name[-3:] == "txt" or self.name[-3:] == "TXT":
 			print "Reading TXT file: %s" % self.name
 			self.textRead()
+			print time.time() - start
 
 
 	def textRead(self):
-		i=0
 		with open(self.name) as f:
-			datas = f.readlines()
-
-			self.txtread = np.array(datas[0].split())
+			for row in f:
+				if self.txtread is None:
+					self.txtread = row.split()
+					continue
+				coba=row.split()
+				cobaNo = np.array([0])
+				for x in coba:
+					cobaNo = np.hstack((cobaNo, float(x)))
+				self.txtread = np.vstack((self.txtread, np.array(cobaNo[1:])))
 			print self.txtread.shape
-			for row in datas:
-				coba=datas[i].split()
-				self.txtread = np.vstack((self.txtread, np.array(coba)))
-				i+=1
-			print self.txtread.shape
-			
-
+			print self.txtread[0]
+			print self.txtread[1]
+			self.hasil = np.array(self.txtread).T
 
 	def readData(self):
 		with open(self.name, 'r') as f:
@@ -54,21 +58,40 @@ class preprocessing:
 
 	def dcOffset(self):
 		i=1
-		for column in self.hasil.T[1:]:
-			self.hasil.T[i][1:] = self.hasil.T[i][1:].astype(np.float) - np.mean(column[1:].astype(np.float))	#DC Offset
-			i+=1
+		if self.name[-3:] == "csv" or self.name[-3:] == "CSV":
+			for column in self.hasil.T[1:]:
+				self.hasil.T[i][1:] = self.hasil.T[i][1:].astype(np.float) - np.mean(column[1:].astype(np.float))	#DC Offset
+				i+=1
+		elif self.name[-3:] == "txt" or self.name[-3:] == "TXT":
+			for column in self.hasil.T[:]:
+				self.hasil.T[i][:] = self.hasil.T[i][:].astype(np.float) - np.mean(column[:].astype(np.float))	#DC Offset
+				i+=1
+
 
 	def fft(self):
 		i = 1
-		for column in self.hasil.T[1:]:
-			if self.ibp is None:
-				self.ibp = np.zeros(len(column) - 1)    
-			# FFT
-			fft = scipy.fft(self.hasil.T[i][1:])
-			for j in range(0, len(fft)):
-				if j>=10:fft[j]=0
-			self.ibp = np.vstack((self.ibp, np.array(scipy.ifft(fft))))
-			i += 1
+
+		if self.name[-3:] == "csv" or self.name[-3:] == "CSV":
+			for column in self.hasil.T[1:]:
+				if self.ibp is None:
+					self.ibp = np.zeros(len(column) - 1)    
+				# FFT
+				fft = scipy.fft(self.hasil.T[i][1:])
+				for j in range(0, len(fft)):
+					if j>=10:fft[j]=0
+				self.ibp = np.vstack((self.ibp, np.array(scipy.ifft(fft))))
+				i += 1
+		elif self.name[-3:] == "txt" or self.name[-3:] == "TXT":
+			for column in self.hasil.T[:]:
+				if self.ibp is None:
+					self.ibp = np.zeros(len(column) - 1)    
+				# FFT
+				fft = scipy.fft(self.hasil.T[i][:])
+				for j in range(0, len(fft)):
+					if j>=10:fft[j]=0
+				self.ibp = np.vstack((self.ibp, np.array(scipy.ifft(fft))))
+				i += 1
+
 		self.ibp=self.ibp.T
 
 
@@ -87,21 +110,37 @@ class preprocessing:
 	def bandpass(self):
 		i=1;
 		cutoff=[1,4,8,12,30]
-		for column in self.hasil.T[1:]:
-			if self.bpass is None:
-				self.bpass = np.zeros(len(column) - 1)
-			if self.bpassABG is None:
-				self.bpassABG = np.zeros(len(column) - 1)
-			bps = self.butter_bandpass_filter(self.hasil.T[i][1:],3,30,128,2)
-			self.bpass= np.vstack((self.bpass, np.array(bps)))
-			i+=1
-		for x in range(0,4):
-			bpastemp = self.butter_bandpass_filter(self.hasil.T[1][1:],cutoff[x],cutoff[x+1],128,2)
-			if(np.array_equal(self.bpassABG,np.zeros(len(column)-1))):
-				self.bpassABG = np.array(bpastemp)
-			else:
-				self.bpassABG= np.vstack((self.bpassABG,np.array(bpastemp)))
-		
+		if self.name[-3:] == "csv" or self.name[-3:] == "CSV":
+			for column in self.hasil.T[1:]:
+				if self.bpass is None:
+					self.bpass = np.zeros(len(column) - 1)
+				if self.bpassABG is None:
+					self.bpassABG = np.zeros(len(column) - 1)
+				bps = self.butter_bandpass_filter(self.hasil.T[i][1:],3,30,128,2)
+				self.bpass= np.vstack((self.bpass, np.array(bps)))
+				i+=1
+			for x in range(0,4):
+				bpastemp = self.butter_bandpass_filter(self.hasil.T[1][1:],cutoff[x],cutoff[x+1],128,2)
+				if(np.array_equal(self.bpassABG,np.zeros(len(column)-1))):
+					self.bpassABG = np.array(bpastemp)
+				else:
+					self.bpassABG= np.vstack((self.bpassABG,np.array(bpastemp)))
+		elif self.name[-3:] == "txt" or self.name[-3:] == "TXT":
+			for column in self.hasil.T[:]:
+				if self.bpass is None:
+					self.bpass = np.zeros(len(column) -1)
+				if self.bpassABG is None:
+					self.bpassABG = np.zeros(len(column) -1)
+				print self.bpass.shape
+				bps = self.butter_bandpass_filter(self.hasil.T[i][:],0.5,30,500,2)
+				self.bpass= np.vstack((self.bpass, np.array(bps)))
+				i+=1
+			for x in range(0,4):
+				bpastemp = self.butter_bandpass_filter(self.hasil.T[1][:],cutoff[x],cutoff[x+1],128,2)
+				if(np.array_equal(self.bpassABG,np.zeros(len(column)-1))):
+					self.bpassABG = np.array(bpastemp)
+				else:
+					self.bpassABG= np.vstack((self.bpassABG,np.array(bpastemp)))
 		self.bpass = self.bpass.T
 		self.bpassABG = self.bpassABG.T
 
@@ -143,5 +182,7 @@ class preprocessing:
 # hihi.plot()
 
 lala = preprocessing('1_rilex_close_pre_bipolar.TXT')
+lala.bandpass()
+lala.plot()
 
 # print proc.LBP1D()
